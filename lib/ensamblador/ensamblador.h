50f676b8e3be6639622b8008a1c33a03d42421e5
fmirandabonomi <83925496+fmirandabonomi@users.noreply.github.com>
2022-04-20 18:55:43 -0300

    Update ensamblador.h

--- lib/ensamblador/ensamblador.h
+++ lib/ensamblador/ensamblador.h
@@ -1,8 +1,8 @@
 #ifndef ENSAMBLADOR_H
 #define ENSAMBLADOR_H
 #include <stddef.h>
-void copiaMemoria(const void *origen,void *destino,size_t cuentaBytes);
-void copiaCadena(const char *origen,char *destino,size_t espacioDestino);
+void copiaMemoria(const void *origen,void *destino,size_t longitud);
+void copiaCadena(const char *origen,char *destino,size_t longitudMaxima);
 void mayusculaEnLugar(char *cadena,size_t longitudMaxima);
 void minusculaEnLugar(char *cadena,size_t longitudMaxima);
 
